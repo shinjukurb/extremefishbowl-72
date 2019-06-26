a53ae37c9f4714ecf102ae43f1fb91b12198d503
threetreeslight <ae06710@gmail.com>
2019-06-26 20:51:11 +0900

    kne feat. tararico

--- card.rb
+++ card.rb
@@ -1,8 +1,10 @@
 class Card
-  attr_reader :mark, :number
+  attr_reader :suit, :number
 
-  def initialize(mark:, number:)
-    @mark = mark
+  SUITS = %w[spade heart club dia]
+
+  def initialize(suit:, number:)
+    @suit = suit
     @number = number
   end
 end
