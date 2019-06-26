b752cfcccd019d3c642a5d1cc6ad10edf804709f
threetreeslight <ae06710@gmail.com>
2019-06-26 21:11:52 +0900

    ttl feat treby

--- deck.rb
+++ deck.rb
@@ -1,11 +1,19 @@
 require_relative './card'
 
 class Deck
+  attr_reader :cards
+
   def initialize
-    Card::SUITS.reduce([]) do |acc, s|
-      acc << Card::NUMBERS.map do |number|
-        
+    @cards = Card::SUITS.reduce([]) do |acc, s|
+      acc << Card::NUMBERS.map do |n|
+        Card.new(s, n)
       end
-    end
+    end.flatten
   end
+
+  private
+
+    def card_count
+      @cards.length
+    end
 end
---
+++ spec/deck_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe Deck do
+  describe "initialize" do
+    it do
+      Deck.new
+    end
+  end
+end
